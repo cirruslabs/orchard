895e6c247933b6990ed706828094079e9319e8a8
Nikolay Edigaryev <edigaryev@gmail.com>
2023-03-23 12:19:03 +0400

    Introduce "orchard logs vm" command

---
+++ internal/command/logs/vm.go
@@ -0,0 +1,37 @@
+package logs
+
+import (
+	"fmt"
+	"github.com/cirruslabs/orchard/pkg/client"
+	"github.com/spf13/cobra"
+)
+
+func newLogsVMCommand() *cobra.Command {
+	command := &cobra.Command{
+		Use:  "vm NAME",
+		RunE: runLogsVM,
+		Args: cobra.ExactArgs(1),
+	}
+
+	return command
+}
+
+func runLogsVM(cmd *cobra.Command, args []string) error {
+	name := args[0]
+
+	client, err := client.New()
+	if err != nil {
+		return err
+	}
+
+	lines, err := client.VMs().Logs(cmd.Context(), name)
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		fmt.Print(line)
+	}
+
+	return nil
+}
