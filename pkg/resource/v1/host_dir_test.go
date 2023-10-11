18a8cf952304639f995695c34ebbc76bdd2a86c2
Nikolay Edigaryev <edigaryev@gmail.com>
2023-10-11 18:02:15 +0400

    HostDir: support URLs in path

--- pkg/resource/v1/host_dir_test.go
+++ pkg/resource/v1/host_dir_test.go
@@ -1,12 +1,13 @@
 package v1_test
 
 import (
+	"fmt"
 	v1 "github.com/cirruslabs/orchard/pkg/resource/v1"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func TestNewHostDirFromString(t *testing.T) {
+func TestHostDir(t *testing.T) {
 	hostDir, err := v1.NewHostDirFromString("large-project:/Users/ci/src/www:ro")
 	require.NoError(t, err)
 	require.EqualValues(t, v1.HostDir{
@@ -22,4 +23,27 @@ func TestHostDirString(t *testing.T) {
 		Path:     "/Users/ci/src/www",
 		ReadOnly: true,
 	}.String())
+}
+
+// TestHostDirWithArchiveURL ensures that Orchard supports "http{,s}://" paths[1].
+//
+// [1]: https://github.com/cirruslabs/tart/pull/620
+func TestHostDirWithArchiveURL(t *testing.T) {
+	const hostDirRaw = "ghar:https://example.com/archive.tar.gz"
+
+	hostDir, err := v1.NewHostDirFromString(hostDirRaw)
+	require.NoError(t, err)
+	require.EqualValues(t, v1.HostDir{
+		Name:     "ghar",
+		Path:     "https://example.com/archive.tar.gz",
+		ReadOnly: false,
+	}, hostDir)
+
+	hostDirReadOnly, err := v1.NewHostDirFromString(fmt.Sprintf("%s:ro", hostDirRaw))
+	require.NoError(t, err)
+	require.EqualValues(t, v1.HostDir{
+		Name:     "ghar",
+		Path:     "https://example.com/archive.tar.gz",
+		ReadOnly: true,
+	}, hostDirReadOnly)
 }
