9c89754118fc6232e6b565a71853ab8d4736c5d8
Nikolay Edigaryev <edigaryev@gmail.com>
2026-02-23 14:49:32 +0100

    test(load): avoid unnecessary new Uint8Array(...)

--- test/load/vm-lifecycle.js
+++ test/load/vm-lifecycle.js
@@ -74,7 +74,7 @@ async function portForward(vmName) {
     });
     ws.binaryType = 'arraybuffer';
 
-    const sentBytes = new Uint8Array(crypto.randomBytes(WS_BYTES));
+    const sentBytes = crypto.randomBytes(WS_BYTES);
     const sentHash = crypto.sha256(sentBytes, 'hex');
     let numReceivedBytes = 0;
     const receivedHasher = crypto.createHash('sha256');
